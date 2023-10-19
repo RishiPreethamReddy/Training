dd7cc40e08f73a47003be3b9a26b636cd37fbdc9
RishiPreethamReddy <146699429+RishiPreethamReddy@users.noreply.github.com>
2023-10-19 09:43:02 +0530

    Add files via upload

---
+++ training/day_2/datatypes.java
@@ -0,0 +1,16 @@
+
+public class datatypes{
+    public static void main(String[] args)
+    {
+        byte age=50;
+        int pincode=633002;
+        long mobile=99999;
+        long aadhar=8888888;
+        long debit=11111111;
+        System.out.println(age);
+        System.out.println(pincode);
+        System.out.println(mobile);
+        System.out.println(aadhar);
+        System.out.println(debit);
+    }
+}
